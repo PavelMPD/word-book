11a9819da37657413363fee5390b4e2b1e2a8005
Pavel <pavelmpd82@gmail.com>
2017-10-14 11:31:16 +0300

    Add the TXT word file parser.

---
+++ word_book/utils/txt_parser.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+class TxtParser(object):
+    WORD = 'word'
+    EXPLANATION = 'explanation'
+    EXAMPLE = 'example'
+    UNKNOWN = 'unknown'
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def parse(self):
+        with open(file=self.filename, mode='rt', encoding='utf-8-sig') as stream:
+            for row in stream:
+                if not row.strip():
+                    continue
+                type_, text = self.get_object(row)
+                yield type_, text
+
+    def get_object(self, row):
+        text = row.strip()
+        type_ = self.get_type(row)
+        return type_, text
+
+    def get_type(self, row):
+        if not re.match('\t', row):
+            return self.WORD
+        elif re.match('\t+:', row):
+            return self.EXPLANATION
+        elif re.match('\t+', row):
+            return self.EXAMPLE
+        return self.UNKNOWN
+
+
+if __name__ == '__main__':
+    parser = TxtParser('./data/word list 20171013.txt')
+    parser.parse()
--- word_book/word/managers/word_manager.py
+++ word_book/word/managers/word_manager.py
@@ -1,8 +1,2 @@
-from word.managers import txt_parser
-
-
 class WordManager(object):
-    def import_from_txt_file(self, filename):
-        parser = txt_parser.TxtParser(filename)
-        for row in parser.parse():
-            print(row)
+    pass
